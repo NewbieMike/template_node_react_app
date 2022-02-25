f4a17a4bc882722020f12daeefa33cb908d49631
Michał Micherda <mmicherdakr@gmail.com>
2022-02-25 10:27:15 +0100

    Add backend plus form

--- client/src/App.js
+++ client/src/App.js
@@ -1,26 +1,95 @@
-// client/src/App.js
-
-import React from "react";
-import logo from "./logo.svg";
+import React, { useState, useEffect } from "react";
 import "./App.css";
+import ContactForm from "./ContactForm";
 
-function App() {
-  const [data, setData] = React.useState(null);
+const App = () => {
+  const [data, setData] = useState(null);
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [subject, setSubject] = useState("");
+  const [message, setMessage] = useState("");
 
-  React.useEffect(() => {
+  useEffect(() => {
     fetch("/api")
       .then((res) => res.json())
       .then((data) => setData(data.message));
   }, []);
 
+  const sendMail = (e) => {
+    e.preventDefault();
+    let formData = {
+      name: name,
+      email: email,
+      subject: subject,
+      message: message,
+    };
+    console.log("sending", formData);
+    let xhr = new XMLHttpRequest();
+    xhr.open("POST", "/");
+    xhr.setRequestHeader("content-type", "application/json");
+    xhr.onload = function () {
+      console.log(xhr.responseText);
+      if (xhr.responseText === "success") {
+        alert("Email sent");
+        setName("");
+        setEmail("");
+        setSubject("");
+        setMessage("");
+        console.log(formData);
+        // name = "";
+        // email = "";
+        // subject = "";
+        // message = "";
+      } else {
+        alert("No sended!");
+      }
+    };
+    xhr.send(JSON.stringify(formData));
+  };
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>{!data ? "Loading..." : data}</p>
-      </header>
+      {data}
+      <h2 className="contact-form-title">Kontakt</h2>
+      <div className="container">
+        <form className="form-container">
+          <input
+            type="text"
+            id="name"
+            placeholder="Imię"
+            required
+            onChange={(event) => setName(event.target.value)}
+          />
+          <input
+            type="email"
+            id="email"
+            placeholder="Adres email"
+            required
+            onChange={(event) => setEmail(event.target.value)}
+          />
+          <input
+            type="text"
+            id="subject"
+            placeholder="Temat wiadomości"
+            required
+            onChange={(event) => setSubject(event.target.value)}
+          />
+          <textarea
+            id="message"
+            placeholder="Wiadomość ..."
+            required
+            onChange={(event) => setMessage(event.target.value)}
+          ></textarea>
+          <input
+            type="submit"
+            className="submit"
+            value="Wyślij"
+            onClick={(e) => sendMail(e)}
+          />
+        </form>
+      </div>
+      <ContactForm />
     </div>
   );
-}
+};
 
 export default App;
--- server/index.js
+++ server/index.js
@@ -1,18 +1,100 @@
 const express = require("express");
 const path = require("path");
-
+const nodemailer = require("nodemailer");
+const cors = require("cors");
 const PORT = process.env.PORT || 3001;
-
+require("dotenv").config();
 const app = express();
+
 // Have Node serve the files for our built React app
 app.use(express.static(path.resolve(__dirname, "../client/build")));
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
 // Handle GET requests to /api route
 app.get("/api", (req, res) => {
   res.json({ message: "Hello from server!" });
 });
+
 // All other GET requests not handled before will return our React app
 app.get("*", (req, res) => {
   res.sendFile(path.resolve(__dirname, "../client/build", "index.html"));
+});
+
+// app.post("/", (req, res) => {
+//   console.log(req.body);
+
+//   const transporter = nodemailer.createTransport({
+//     service: "gmail",
+//     auth: {
+//       user: "mmicherdacsk@gmail.com",
+//       pass: "90michal96!@",
+//     },
+//   });
+
+//   const mailOptions = {
+//     from: req.body.email,
+//     to: "mmicherdacsk@gmail.com",
+//     subject: `Wiadomość od ${req.body.email}: ${req.body.subject}`,
+//     text: req.body.message,
+//   };
+
+//   transporter.sendMail(mailOptions, (error, info) => {
+//     if (
+//       req.body.message === "" ||
+//       req.body.subject === "" ||
+//       req.body.email === "" ||
+//       req.body.name === ""
+//     ) {
+//       res.send(error);
+//     } else {
+//       transporter.sendMail(mailOptions, (error, info) => {
+//         if (error) {
+//           res.send("error");
+//         } else {
+//           res.send("success");
+//         }
+//       });
+//     }
+//   });
+// });
+const transporter = nodemailer.createTransport({
+  service: "gmail",
+  auth: {
+    user: process.env.MAIL,
+    pass: process.env.PASS,
+  },
+});
+transporter.verify(function (error, success) {
+  if (error) {
+    console.log(error);
+  } else {
+    console.log("Server is ready to take our messages");
+  }
+});
+
+app.post("/send", (req, res, next) => {
+  console.log(req.body);
+
+  var mail = {
+    from: req.body.email,
+    to: "mmicherdacsk@gmail.com",
+    subject: `Wiadomość od ${req.body.email}: ${req.body.subject}`,
+    text: req.body.message,
+  };
+
+  transporter.sendMail(mail, (err, data) => {
+    if (err) {
+      res.json({
+        status: "fail",
+      });
+    } else {
+      res.json({
+        status: "success",
+      });
+    }
+  });
 });
 app.listen(PORT, () => {
   console.log(`Server listening on ${PORT}`);
